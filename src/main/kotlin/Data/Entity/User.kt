64db7312ea99f074a2e9fe96e6d9010d057b97c1
gmasalskih <gmasalskih@mail.ru>
2018-04-16 22:26:45 +0300

    AddPerson v1.1.0

---
+++ src/main/kotlin/Data/Entity/User.kt
@@ -0,0 +1,53 @@
+package Data.Entity
+
+import javax.annotation.Generated
+import com.google.gson.annotations.SerializedName
+
+@Generated("com.robohorse.robopojogenerator")
+data class User(
+
+	@field:SerializedName("pstatus")
+	val pstatus: Int? = null,
+
+	@field:SerializedName("rspostidname")
+	val rspostidname: String? = null,
+
+	@field:SerializedName("pipassword")
+	val pipassword: String? = null,
+
+	@field:SerializedName("isuser")
+	val isuser: Boolean? = null,
+
+	@field:SerializedName("plastname")
+	val plastname: String? = null,
+
+	@field:SerializedName("pfirstname")
+	val pfirstname: String? = null,
+
+	@field:SerializedName("pextcode")
+	val pextcode: String? = null,
+
+	@field:SerializedName("personemail")
+	val personemail: String? = null,
+
+	@field:SerializedName("caidname")
+	val caidname: String? = null,
+
+	@field:SerializedName("caid")
+	val caid: String? = null,
+
+	@field:SerializedName("ppsex")
+	val ppsex: String? = null,
+
+	@field:SerializedName("personid")
+	val personid: String? = null,
+
+	@field:SerializedName("psurname")
+	val psurname: String? = null,
+
+	@field:SerializedName("pilogin")
+	val pilogin: String? = null,
+
+	@field:SerializedName("rspostid")
+	val rspostid: String? = null
+)
